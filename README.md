cb983dec307141fc8f37a26f761305951b908443
Domas Šiaudvytis <dsiaudvytis@nexient.com>
2021-02-11 18:26:46 -0500

    travel app

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # fictional-telegram
 
 My collection of dribble immitations, I guess, using Gatsby for some reason.
+
+001 - [Travel App](https://dribbble.com/shots/15048648)
---
+++ src/pages/001.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from './001.module.css';
+
+const Dot = ({ selected = false }) => selected ? (
+  <div className={styles.dot} />
+) : (
+    <div className={styles.greenBorder} >
+      <div className={styles.greenDot} />
+    </div>
+  );
+
+export default () => (
+  <div className={styles.container}>
+    <div className={styles.header}>
+      <div className={styles.backButton}>&lt;</div>
+      <div className={styles.settingsButton}>O</div>
+    </div>
+    <div className={styles.mid}>
+      <div className={styles.card}>
+        <div className={styles.rating}>★ 4.9
+          <div className={styles.outOf}>/5</div>
+        </div>
+        <div className={styles.discover}>
+          Discover
+        </div>
+        <div className={styles.title}>
+          Arches National Park
+        </div>
+        <div className={styles.dots}>
+          {Array.from({ length: 9 }, (_, n) => (<Dot selected={n === 3} />))}
+        </div>
+      </div>
+    </div>
+    <div className={styles.footer}>
+      <div className={styles.button}></div>
+      <div className={styles.button}></div>
+      <div className={styles.button}></div>
+    </div>
+  </div>
+);
